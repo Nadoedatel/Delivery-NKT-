382e5a82e7086d316826af2190fb08f459d33911
Nikita <nikita.vorontsov.1989@mail.ru>
2025-03-06 22:37:59 +0300

    Добавил что бы данные о пользователе подтягивались с БД и выводились в профиль аккаунта, так проверка есть

--- src/main/java/com/deliveryfood/pet/controller/MainController.java
+++ src/main/java/com/deliveryfood/pet/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.deliveryfood.pet.controller;
 
+import com.deliveryfood.pet.models.MyUsers;
 import com.deliveryfood.pet.models.Post;
 import com.deliveryfood.pet.models.Shops;
+import com.deliveryfood.pet.models.User;
 import com.deliveryfood.pet.repo.PostRepository;
 import com.deliveryfood.pet.repo.ShopsRepository;
+import com.deliveryfood.pet.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -17,6 +23,9 @@ public class MainController {
 
     @Autowired
     private ShopsRepository shopsRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String indexPost(Model model){
@@ -28,8 +37,19 @@ public class MainController {
     }
 
     @GetMapping("/profile")
-    public String profile(Model model){
-        model.addAttribute("title", "Профиль пользователя");
-        return "profile";
+    public String profile(Model model, Principal principal){
+        if (principal == null) {
+            return "redirect:/login";
+        }
+
+        String username = principal.getName();
+        Optional<MyUsers> users = userRepository.findByUsername(username);
+
+        if (users.isPresent()) {
+            model.addAttribute("users", users.get());
+            return "profile";
+        } else {
+            return "redirect:/login";
+        }
     }
 }
